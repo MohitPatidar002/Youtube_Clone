697e0eafbb92343275eed84d0105608adf2f21fb
Mohit Patidar <mohitpatidar002@gmail.com>
2024-02-14 19:24:00 +0530

    write utils code

--- src/db/dbconnection.js
+++ src/db/dbconnection.js
@@ -11,4 +11,4 @@ const dbConnect = async () => {
     }
 };
 
-exports = dbConnect();
+module.exports = dbConnect;
--- src/index.js
+++ src/index.js
@@ -1,11 +1,16 @@
 
 require("dotenv").config();
 const express = require("express");
-const { dbConnect } = require("./db/dbconnection");
+const dbConnect = require("./db/dbconnection");
 const app = express();
 
-dbConnect;
-
-app.listen(process.env.PORT,(req,res) => {
-    console.log("app started")
+dbConnect()
+.then(() => {
+    app.listen(process.env.PORT || 8000,(req,res) => {
+        console.log("app started at port ", process.env.PORT)
+    })
 })
+.catch((err) => {
+    console.log("Port err", err)
+})
+
